8a424e8e964128a17f1aedae266ac2e620723773
Anand Sharma <sharmaanand541@gmail.com>
2024-01-21 10:48:31 +0530

    default value

--- Backend/app.js
+++ Backend/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const bodyParser = require("body-parser");
 const app = express();
-const port = process.env.APP_PORT || 3000;
+const port = process.env.APP_PORT || 8080;
 
 // Body parser middleware
 app.use(bodyParser.json());
--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -2,11 +2,11 @@ const mysql = require("mysql2/promise");
 require("dotenv").config();
 
 const pool = mysql.createPool({
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
-  port: process.env.PORT,
+  host: process.env.HOST || "sql12.freemysqlhosting.net",
+  user: process.env.USER || "sql12678433",
+  password: process.env.PASSWORD || "5ISxeDd8xF",
+  database: process.env.DATABASE || "sql12678433",
+  port: process.env.PORT || "3306",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
